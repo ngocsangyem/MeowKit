2ba49588e7af5d55457c7ccef68ae62c3f7949ea
ngocsangyem <nnsang24@gmail.com>
2026-05-15 21:43:34 +0700

    chore: update nuxt configuration settings

--- packages/landing/nuxt.config.ts
+++ packages/landing/nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
       crawlLinks: true,
       routes: ['/'],
     },
+    preset: 'vercel',
   },
 
   // ─── Site / SEO ─────────────────────────────────────────────────────────
